9cff83904fca116924f84325c6a5b0a34af79914
1 <a1@1deMacBook-Pro.local>
2018-02-06 00:44:09 +0800

    test

--- src/common/login.vue
+++ src/common/login.vue
@@ -7,12 +7,11 @@
 </template>
 <script>
 import Cookie from '../service/cookie.js'
+import Login from '../service/login.js';
 export default {
 	methods: {
 		login() {
-			Cookie.setCookie('url', window.location.href)
-			//window.location = "http://pass.muxixyz.com?landing=blog.muxixyz.com/landing"
-			window.location = "http://pass.muxixyz.com?landing=localhost:3000/landing"
+			Login.toLoginPage();
 		}
 	}
 }
